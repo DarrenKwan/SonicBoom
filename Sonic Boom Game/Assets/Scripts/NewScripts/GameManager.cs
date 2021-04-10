d6b2115b36ce6f45134d6ea04ba7958c1c9fb281
simoncurtissmith <ssmith1130@inside.artcenter.edu>
2021-04-10 01:43:11 -0700

    Pushing before testing leap motion integration
    added generic pieces of a test level

---
+++ Sonic Boom Game/Assets/Scripts/NewScripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Handle Enemies")]
+    public List<GameObject> enemies = new List<GameObject>();
+    public Transform[] spawnPos;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
