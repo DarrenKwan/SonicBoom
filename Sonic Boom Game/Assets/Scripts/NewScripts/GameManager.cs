1cdb3c22a57c13924734cd8fbd79d99f16f505ba
simoncurtissmith <ssmith1130@inside.artcenter.edu>
2021-04-12 19:32:41 -0700

    Shooting implemented
    Making game out of scene:
    InteractionObjects1
    
    Can find in:
    >Assets
    >Scenes
    >NewScenes
    >InteractionObjects1

--- Sonic Boom Game/Assets/Scripts/NewScripts/GameManager.cs
+++ Sonic Boom Game/Assets/Scripts/NewScripts/GameManager.cs
@@ -2,20 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace Leap.Unity.Interaction
 {
-    [Header("Handle Enemies")]
-    public List<GameObject> enemies = new List<GameObject>();
-    public Transform[] spawnPos;
-
-
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        
-    }
+        [Header("Handle Enemies")]
+        public List<GameObject> enemies = new List<GameObject>();
+        public Transform[] spawnPos;
 
-    void Update()
-    {
+        [Header("LeapMotionStuff")]
+        Transform firePos_L, firePos_R;
+        [SerializeField] GameObject redBullet, blueBullet;
+        bool canFire_L, canFire_R;
+        [SerializeField] float firingCooldown = 1.5f;
+
+        RiggedHand[] hand = new RiggedHand[2];
         
+
+        private void Awake()
+        {
+            //left hand
+            if(hand[0] == null)
+            {
+                hand[0] = GameObject.Find("LoPoly Rigged Hand Left").GetComponent<RiggedHand>();
+            }
+
+            //right hand
+            if(hand[1] == null)
+            {
+                hand[1] = GameObject.Find("LoPoly Rigged Hand Right").GetComponent<RiggedHand>();
+            }   
+
+            if (firePos_L == null)
+            {
+                //firePos_L = GameObject.Find("L_Palm").transform;
+                firePos_L = GameObject.Find("FirePos_L").transform;
+            }
+
+            if (firePos_R == null)
+            {
+                //firePos_R = GameObject.Find("R_Palm").transform;
+                firePos_R = GameObject.Find("FirePos_R").transform;
+            }
+        }
+
+        void Start()
+        {
+            canFire_L = true;
+            canFire_R = true;
+        }
+
+        public void CheckIfFired_R()
+        {
+            if (canFire_R)
+            {
+                StartCoroutine(Fire_R());
+            }
+        }
+
+        public void CheckIfFired_L()
+        {
+            if (canFire_L)
+            {
+                StartCoroutine(Fire_L());
+            }
+        }
+
+        IEnumerator Fire_R()
+        {
+            canFire_R = false;
+
+            GameObject bul_R = Instantiate(redBullet, firePos_R.position, Quaternion.identity);
+
+            //Instantiate(bul_R);
+            bul_R.GetComponent<Rigidbody>().AddForce(0, 0, 10f, ForceMode.Impulse);
+
+            Debug.Log("Fire from your right hand.");
+            yield return new WaitForSeconds(firingCooldown);
+            canFire_R = true;
+        }
+
+        IEnumerator Fire_L()
+        {
+            canFire_L = false;
+
+            GameObject bul_L = Instantiate(blueBullet, firePos_L.position, Quaternion.identity);
+
+            //Instantiate(bul_L);
+            bul_L.GetComponent<Rigidbody>().AddForce(0, 0, 10f, ForceMode.Impulse);
+
+            Debug.Log("Fire from your left hand.");
+            yield return new WaitForSeconds(firingCooldown);
+            canFire_L = true;
+        }
+
     }
 }
---
+++ Sonic Boom Game/Assets/Scripts/NewScripts/HandTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+[RequireComponent(typeof(InteractionBehaviour))]
+public class HandTrigger : MonoBehaviour
+{
+    private InteractionBehaviour _intObj;
+
+    private void Start()
+    {
+        _intObj = GetComponent<InteractionBehaviour>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hand"))
+        {
+            Debug.Log("Shoot a thing");
+        }
+    }
+}
